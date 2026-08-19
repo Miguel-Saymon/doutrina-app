23dc0fc0a9fafca813b9e04a86d767a08e324597
Miguel Saymon <miguel.saymon05@gmail.com>
2026-08-19 18:38:22 -0300

    chore: initialize Expo project

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>doutrina.net</Text>
+      <Text style={styles.subtitle}>Aplicativo em desenvolvimento</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: '600',
+  },
+  subtitle: {
+    marginTop: 8,
+    fontSize: 16,
+  },
+});
